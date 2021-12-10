6c077a07a23942f01cb0f1e5761a8bb36c916260
Jessica Flobeck <jessicalaynefly@gmail.com>
2021-12-10 09:36:21 -0600

    wrote app to hit Giphy API with user form inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SearchResults from "./SearchResults";
+// import "./App.css";
 
 function App() {
+  const [userQuery, setUserQuery] = useState("");
+  const [userLimit, setUserLimit] = useState(10);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserQuery(e.target.search.value);
+    setUserLimit(e.target.limit.value);
+    console.log(userQuery);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center">
+      <div className="p-6 w-1/2 bg-white rounded-xl shadow-md text-center m-auto mt-4 mb-4">
+        <form onSubmit={handleSubmit} className="mx-w-full">
+          <label for="gif-search" className="font-semibold text-lg">
+            Giphy Search
+          </label>
+          <input
+            type="text"
+            name="search"
+            className="text-md font-medium border-grey-300 border-2 rounded-md p-1 mt-1 w-full"
+          ></input>
+          <label for="limit" className="m-2">
+            Number of Results
+          </label>
+          <input
+            type="number"
+            name="limit"
+            min="1"
+            max="30"
+            className="text-md font-medium border-grey-300 border-2 rounded-md p-1 mt-1 w-auto"
+          ></input>
+          <button
+            type="submit"
+            className="block bg-green-900 bg-opacity-70 rounded-2xl px-2 mt-2 m-auto"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+      {userQuery ? (
+        <p className="m-auto font-medium mb-4">{`You searched for ${userQuery}`}</p>
+      ) : (
+        <p></p>
+      )}
+      <SearchResults userQuery={userQuery} userLimit={userLimit} />
     </div>
   );
 }
---
+++ src/SearchResults.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const SearchResults = ({ userQuery, userLimit }) => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [gifs, setGifs] = useState([]);
+
+  const baseUrl =
+    "https://api.giphy.com/v1/gifs/search?api_key=AimnYfTfv0IUpTe0jgXcml0Af0r3K8P9";
+
+  useEffect(() => {
+    fetch(`${baseUrl}&q=${userQuery}&limit=${userLimit}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+
+          setGifs(result.data);
+          //   console.log(userQuery);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [gifs, userQuery]);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul className="flex flex-row flex-wrap w-1/2 m-auto">
+        {gifs.map((gif) => (
+          <li key={gif.id} className="m-2">
+            <img
+              src={`https://i.giphy.com/media/${gif.id}/100.gif`}
+              alt={gif.id}
+            ></img>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+};
+
+export default SearchResults;
