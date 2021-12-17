bd4f04302b315738f6eb1754fcfad51ae2129540
Jessica Flobeck <jessicalaynefly@gmail.com>
2021-12-17 09:25:28 -0600

    update tailwind order, add form clearing and rename state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import SearchResults from "./SearchResults";
 // import "./App.css";
 
 function App() {
-  const [userQuery, setUserQuery] = useState("");
+  const [gifQuery, setGifQuery] = useState("");
   const [userLimit, setUserLimit] = useState(10);
   const handleSubmit = (e) => {
     e.preventDefault();
-    setUserQuery(e.target.search.value);
+    setGifQuery(e.target.search.value);
     setUserLimit(e.target.limit.value);
-    console.log(userQuery);
+    e.target.reset();
   };
 
   return (
     <div className="flex flex-col justify-center">
-      <div className="p-6 w-1/2 bg-white rounded-xl shadow-md text-center mx-auto my-4">
+      <div className="mx-auto my-4 rounded-xl shadow-md p-6 w-1/2 bg-white text-center">
         <form onSubmit={handleSubmit} className="mx-w-full">
           <label for="gif-search" className="font-semibold text-lg">
             Giphy Search
@@ -22,7 +22,7 @@ function App() {
           <input
             type="text"
             name="search"
-            className="text-md font-medium border-grey-300 border-2 rounded-md p-1 mt-1 w-full"
+            className="mt-1 rounded-md border-grey-300 border-2 p-1 w-full text-md font-medium"
           ></input>
           <label for="limit" className="m-2">
             Number of Results
@@ -32,22 +32,22 @@ function App() {
             name="limit"
             min="1"
             max="30"
-            className="text-md font-medium border-grey-300 border-2 rounded-md p-1 mt-1 w-auto"
+            className="mt-1 border-grey-300 border-2 rounded-md p-1 w-auto text-md font-medium"
           ></input>
           <button
             type="submit"
-            className="block bg-green-900 bg-opacity-70 rounded-2xl px-2 mt-2 m-auto"
+            className="block mt-2 m-auto rounded-2xl px-2 bg-green-900 bg-opacity-70"
           >
             Search
           </button>
         </form>
       </div>
-      {userQuery ? (
-        <p className="m-auto font-medium mb-4">{`You searched for ${userQuery}`}</p>
+      {gifQuery ? (
+        <p className="m-auto mb-4 font-medium">{`You searched for ${gifQuery}`}</p>
       ) : (
         <p></p>
       )}
-      <SearchResults userQuery={userQuery} userLimit={userLimit} />
+      <SearchResults gifQuery={gifQuery} userLimit={userLimit} />
     </div>
   );
 }
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const SearchResults = ({ userQuery, userLimit }) => {
+const SearchResults = ({ gifQuery, userLimit }) => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [gifs, setGifs] = useState([]);
@@ -9,31 +9,31 @@ const SearchResults = ({ userQuery, userLimit }) => {
     "https://api.giphy.com/v1/gifs/search?api_key=AimnYfTfv0IUpTe0jgXcml0Af0r3K8P9";
 
   useEffect(() => {
-      if (userQuery && userLimit) {
-    fetch(`${baseUrl}&q=${userQuery}&limit=${userLimit}`)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
+    if (gifQuery && userLimit) {
+      fetch(`${baseUrl}&q=${gifQuery}&limit=${userLimit}`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
 
-          setGifs(result.data);
-          //   console.log(userQuery);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+            setGifs(result.data);
+            //   console.log(userQuery);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
     }
-  }, [gifs, userQuery, userLimit]);
+  }, [gifs, gifQuery, userLimit]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div className="text-center">Loading...</div>;
   } else {
     return (
-      <ul className="flex flex-row flex-wrap w-1/2 m-auto">
+      <ul className="m-auto flex flex-row flex-wrap w-1/2">
         {gifs.map((gif) => (
           <li key={gif.id} className="m-2">
             <img
